ab445aaca629180287636f1bad2b201b2d6c6d8a
Kevin <ksandy@outlook.ie>
2020-01-23 07:29:29 +0000

    add jquery modals

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -6,3 +6,13 @@ arrowJump.addEventListener('click', jumpToPosition);
 function jumpToPosition(){
     flexImages.scrollIntoView({behavior: "smooth"});
 }
+
+flexImages.addEventListener('click', modalFade)
+
+function modalFade(){
+    $('#fadeDelay').modal({
+        fadeDuration: 250,
+        fadeDelay: 0.80
+    });
+}
+
