3885e65ccb610c1c72f2173eacf2796a07b30c8b
Kevin <ksandy@outlook.ie>
2020-01-23 07:48:21 +0000

    add more modals to page images

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,18 +1,34 @@
 var arrowJump = document.getElementById("arrow");
-var flexImages = document.getElementById("flexImages");
+var flexImages1 = document.getElementById("flexImages1");
+var flexImages2 = document.getElementById("flexImages2");
+var flexImages3 = document.getElementById("flexImages3");
 
 arrowJump.addEventListener('click', jumpToPosition);
 
 function jumpToPosition(){
-    flexImages.scrollIntoView({behavior: "smooth"});
+    flexImages1.scrollIntoView({behavior: "smooth"});
 }
 
-flexImages.addEventListener('click', modalFade)
+flexImages1.addEventListener('click', modalFade)
+flexImages2.addEventListener('click', modalFade)
+flexImages3.addEventListener('click', modalFade)
 
-function modalFade(){
-    $('#fadeDelay').modal({
+function modalFade(event){
+    if(event.target === flexImages1){
+        $('#fadeDelay1').modal({
         fadeDuration: 250,
         fadeDelay: 0.80
-    });
+        });
+    }else if(event.target === flexImages2){
+        $('#fadeDelay2').modal({
+        fadeDuration: 250,
+        fadeDelay: 0.80
+        });
+    }else{
+        $('#fadeDelay3').modal({
+        fadeDuration: 250,
+        fadeDelay: 0.80
+        }); 
+    }
 }
 
