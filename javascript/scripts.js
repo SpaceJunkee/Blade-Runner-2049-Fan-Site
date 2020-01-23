94d17a3efbcfb3ff0966e267d755765ca169c571
Kevin <ksandy@outlook.ie>
2020-01-23 23:18:12 +0000

    add icon to nav bar, flex box to nav bar,

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -7,7 +7,8 @@ arrowJump.addEventListener('click', jumpToPosition);
 
 //Jump to position on page
 function jumpToPosition(){
-    flexImages1.scrollIntoView({behavior: "smooth"});
+    flexImages1.scrollIntoView({behavior: "smooth", block: 'center',
+            inline: 'center'});
 }
 
 flexImages1.addEventListener('click', modalFade);
