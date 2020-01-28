8c0aae47203d30efc3c5e32b663fef1e69d2c66f
Kevin Sandy <ksandy@outlook.ie>
2020-01-28 15:32:30 +0000

    change nav colours on joi page

---
+++ javascript/joiPageScript.js
@@ -0,0 +1,49 @@
+var arrowJump = document.getElementById("arrowDown");
+
+arrowJump.addEventListener('click', jumpToPosition);
+
+var middlePageJump = document.getElementById("middlePageImage");
+
+//Jump to position on page
+function jumpToPosition(){
+    middlePageJump.scrollIntoView({behavior: "smooth", block: 'center',inline: 'center'});
+}
+
+
+/*
+//fade in modal depending on which image is clicked
+function modalFade(event){
+    if(event.target === flexImages1){
+        $('#fadeDelay1').modal({
+        fadeDuration: 250,
+        fadeDelay: 0.80
+        });
+    }else if(event.target === flexImages2){
+        $('#fadeDelay2').modal({
+        fadeDuration: 250,
+        fadeDelay: 0.80
+        });
+    }else{
+        $('#fadeDelay3').modal({
+        fadeDuration: 250,
+        fadeDelay: 0.80
+        }); 
+    }
+}*/
+
+//SideBar Functionality
+
+var closeButton = document.getElementById("closeButton");
+var sideBar = document.getElementById("sideBarContainer");
+
+closeButton.addEventListener("click", closeSideBar);
+
+sideBar.addEventListener("click", openSideBar);
+
+function openSideBar() {
+  document.getElementById("mySideBar").style.width = "80px";
+}
+
+function closeSideBar() {
+  document.getElementById("mySideBar").style.width = "0";
+}
